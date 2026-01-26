2fd5769a51fdd98f713a93f152d32575822b6f7b
wei-song1 <weisong141@hotmail.com>
2026-01-26 16:08:22 +0800

    Test Github Pages 2

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+
+  basePath: "/hack-n-roll",
+  assetPrefix: "/hack-n-roll/",
+
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
