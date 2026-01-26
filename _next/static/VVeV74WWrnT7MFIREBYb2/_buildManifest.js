13ec21c8bab82fbd6ccc1f8eeaa71081311934ad
wei-song1 <weisong141@hotmail.com>
2026-01-26 16:28:41 +0800

    Updates

--- _next/static/VVeV74WWrnT7MFIREBYb2/_buildManifest.js
+++ _next/static/VVeV74WWrnT7MFIREBYb2/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/hack-n-roll//_next/:path+",
-        "destination": "/hack-n-roll/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
